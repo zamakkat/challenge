ac6ab9ac2239139db7ee99aad7042de3b8d4ef02
Duong Ho Tuan <zamakkat@gmail.com>
2013-12-01 01:40:00 +0800

    Init Project with gitignore, bootstrap, postgreSQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Challenge::Application.config.secret_key_base = '0dc9c440d6e26f158538b6d0ae513073b1e158e4be1be9e22c93aa5388df7f55c1078c8209e985df08fba290a7dbe45c987fe52adb4b8a2af9b5f5cee8656abf'
